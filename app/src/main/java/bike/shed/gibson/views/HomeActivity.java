ab16d04f210a501e3911ce59f59551126f24bf2d
Paul Tagliamonte <tag@pault.ag>
2014-11-02 10:36:37 -0500

    Move some stuffa around.

---
+++ app/src/main/java/bike/shed/gibson/views/HomeActivity.java
@@ -0,0 +1,21 @@
+package bike.shed.gibson.views;
+
+/**
+ *
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import bike.shed.gibson.R;
+
+/**
+ *
+ */
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
